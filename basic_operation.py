e2867d9717fd5af0ce5ee9f9397c96fd478ed726
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2025-07-24 11:29:13 +0530

    basic operation like crop resize using pillow

---
+++ basic_operation.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+img = Image.open(r"C:\\Users\Soumyajit Koley\Downloads\OIP.jpeg")
+img.show()
+# resizeing operation ....................
+
+resized_img = img.resize((200,200))
+
+resized_img.show()
+
+# cropping operation .....................
+
+cropped_img = img.crop((10,100,100,220))
+
+cropped_img.show()
